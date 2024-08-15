32bbcfe63dbbdf692244c65b69339118e259330c
bharathdhfm <151800216+bharathdhfm@users.noreply.github.com>
2024-08-15 08:36:06 +0530

    Add files via upload

---
+++ LanfingPage/script.js
@@ -0,0 +1,9 @@
+// Add scroll event listener
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
